2a9dd11b5c4045392bae22c7dc15e67e6826ebd3
MRGRD56 <bukinist85@gmail.com>
2020-10-22 12:05:12 +0300

    Добавлен CheckMark(), написаны тесты.

---
+++ RegMarkLibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RegMarkLibTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegMarkLibTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("FD9228C9-9F02-422D-A6EC-74F7E9417984")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RegMarkLibTest/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using REG_MARK_LIB;
+
+namespace RegMarkLibTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void CheckMarkTest()
+        {
+            Assert.True(RegMark.CheckMark("A111AA777"));
+            Assert.True(RegMark.CheckMark("A252KH716"));
+            Assert.False(RegMark.CheckMark("Q134JD788 "));
+        }
+    }
+}
